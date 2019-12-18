ae7772631087372516e852216b6b3558944b3c2b
HarryLNRD <compatflame9@hotmail.gr>
2019-12-19 00:51:44 +0200

    Add and configure bootstrap gem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery").start()
-require("bootstrap-sprockets").start()
 
 
 
